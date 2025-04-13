e475e3e57bd56a537555b32052117ea763ad650e
Aleksandr <sanchezzkusika@gmail.com>
2025-04-13 23:13:09 +0300

    the admin panel is configured

--- blogicum_2/blogicum/blog/admin.py
+++ blogicum_2/blogicum/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post, Category, Location
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'slug', 'is_published', 'created_at')
+        }),
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location)
+admin.site.register(Post)
+
+admin.site.site_title = "Админка Блога"
+admin.site.site_header = "Блог"
+admin.site.index_title = "Панель управления"
