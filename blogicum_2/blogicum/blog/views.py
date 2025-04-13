fdb9a69f388d4ab8efb4ec846ce94c1c81d43a64
Aleksandr <sanchezzkusika@gmail.com>
2025-04-13 22:54:38 +0300

    view functions update

--- blogicum_2/blogicum/blog/views.py
+++ blogicum_2/blogicum/blog/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post, Category
+
 
 def index(request):
-    reversed_posts = list(reversed(posts))
-    return render(request, 'blog/index.html', {'posts': reversed_posts})
-
-
-def post_detail(request, id):
-    post = next((p for p in posts if p['id'] == id), None)
-    return render(request, 'blog/detail.html', {'post': post})
+    qs = Post.objects.filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+    return render(request, 'blog/index.html', {'post_list': qs})
 
 
 def category_posts(request, category_slug):
-    filtered_posts = [
-        post for post in posts if post['category'] == category_slug]
+    category = get_object_or_404(
+        Category, slug=category_slug, is_published=True)
+    qs = Post.objects.filter(
+        category=category,
+        pub_date__lte=timezone.now(),
+        is_published=True
+    ).order_by('-pub_date')
     return render(request, 'blog/category.html', {
-        'category_slug': category_slug,
-        'posts': filtered_posts,
+        'category': category,
+        'post_list': qs,
     })
+
+
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post,
+        pk=id,
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
+    )
+    return render(request, 'blog/detail.html', {'post': post})
