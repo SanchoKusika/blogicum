3a27e0eaeafe34e2c7b032af1e4d6a98cf37d7dc
Aleksandr <sanchezzkusika@gmail.com>
2025-04-14 21:32:25 +0300

    all paths in blog have been rewritten

--- blogicum_3/blogicum/blog/urls.py
+++ blogicum_3/blogicum/blog/urls.py
@@ -1,36 +1,64 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.urls import include, path
+from blog import views
 
 app_name = 'blog'
 
+posts_urls = [
+    path(
+        '<int:post_id>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
+    path(
+        'create/',
+        views.PostCreateView.as_view(),
+        name='create_post'
+    ),
+    path(
+        '<int:post_id>/edit/',
+        views.PostUpdateView.as_view(),
+        name='edit_post'
+    ),
+    path(
+        '<int:post_id>/delete/',
+        views.PostDeleteView.as_view(),
+        name='delete_post'
+    ),
+
+
+    path('<int:post_id>/comment/',
+         views.CommentCreateView.as_view(),
+         name='add_comment'),
+    path(
+        '<int:post_id>/edit_comment/<int:comment_id>/',
+        views.CommentUpdateView.as_view(),
+        name='edit_comment'
+    ),
+    path(
+        '<int:post_id>/delete_comment/<int:comment_id>/',
+        views.CommentDeleteView.as_view(),
+        name='delete_comment'
+    ),
+]
+
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/',
-         views.category_posts, name='category_posts'),
-    path('create/', views.create_post, name='create_post'),
-    path('edit/<int:id>/', views.edit_post, name='edit_post'),
-    path('delete/<int:id>/', views.delete_post, name='delete_post'),
-    path('posts/<int:id>/add_comment/', views.add_comment, name='add_comment'),
-    path('posts/<int:post_id>/edit_comment/<int:comment_id>/',
-         views.edit_comment, name='edit_comment'),
-    path('posts/<int:post_id>/delete_comment/<int:comment_id>/',
-         views.delete_comment, name='delete_comment'),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(
-        template_name='registration/logged_out.html'), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(
-        template_name='registration/password_change_form.html'), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='registration/password_change_done.html'), name='password_change_done'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='registration/password_reset_form.html'), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='registration/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
-    path('registration/', views.register, name='registration'),
+    path('', views.PostListView.as_view(), name='index'),
+    path(
+        'category/<slug:post_category>/',
+        views.CategoryListView.as_view(),
+        name='category_posts'
+    ),
+
+    path(
+        'profile/<slug:username>/',
+        views.ProfileDetailView.as_view(),
+        name='profile'
+    ),
+    path(
+        'edit_profile/',
+        views.UserUpdateView.as_view(),
+        name='edit_profile'
+    ),
+    path('posts/', include(posts_urls)),
 ]
