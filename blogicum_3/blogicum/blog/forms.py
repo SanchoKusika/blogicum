f9a95e36402b22ee7a1836495aa8be0684ac7695
Aleksandr <sanchezzkusika@gmail.com>
2025-04-14 20:12:02 +0300

    the forms were rewritten

--- blogicum_3/blogicum/blog/forms.py
+++ blogicum_3/blogicum/blog/forms.py
@@ -1,26 +1,33 @@
 from django import forms
-from .models import Post, Comment
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from blog.models import Comment, Post, User
 
 
-class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True, label="Электронная почта")
+class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ('username', 'first_name', 'last_name', 'email')
 
 
 class PostForm(forms.ModelForm):
+
     class Meta:
         model = Post
-        fields = ['title', 'text', 'pub_date', 'location', 'category', 'image']
+        exclude = ('author',)
+        widgets = {
+            'pub_date': forms.DateTimeInput(
+                format='%Y-%m-%d %H:%M', attrs={'type': 'datetime-local'}
+            ),
+        }
 
 
 class CommentForm(forms.ModelForm):
+
     class Meta:
         model = Comment
-        fields = ['text']
+        fields = ('text',)
+        widgets = {
+            'text': forms.Textarea(
+                attrs={'cols': 10, 'rows': 4})
+        }
