3a21d01510ac66634a5d826ff96e13ac735ec0cc
Aleksandr <sanchezzkusika@gmail.com>
2025-04-14 17:56:18 +0300

    added registration form

--- blogicum_3/blogicum/blog/forms.py
+++ blogicum_3/blogicum/blog/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 from .models import Post, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Электронная почта")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
 
 
 class PostForm(forms.ModelForm):
