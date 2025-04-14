6b8e2041ec2cbdce21ba5ff07a0a95e604f41459
Aleksandr <sanchezzkusika@gmail.com>
2025-04-14 20:24:07 +0300

    added functions for error handling

--- blogicum_3/blogicum/pages/views.py
+++ blogicum_3/blogicum/pages/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def about(request):
-    return render(request, 'pages/about.html')
+class RulesTemplateView(TemplateView):
+    template_name = 'pages/rules.html'
 
 
-def rules(request):
-    return render(request, 'pages/rules.html')
+class AboutTemplateView(TemplateView):
+    template_name = 'pages/about.html'
+
+
+def csrf_failure(request):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def page_not_found(request):
+    return render(request, 'pages/404.html', status=404)
+
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
