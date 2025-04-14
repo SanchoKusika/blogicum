9d6945b64d1bb47799e38b1a94a75c4770aa636b
Aleksandr <sanchezzkusika@gmail.com>
2025-04-14 21:59:57 +0300

    added new paths in urlpatterns

--- blogicum_3/blogicum/blogicum/urls.py
+++ blogicum_3/blogicum/blogicum/urls.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from blog.views import UserCreateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Админка Django
-    path('', include('blog.urls')),  # Маршруты из приложения blog
-    path('pages/', include('pages.urls')),  # Маршруты из приложения pages
+    path('admin/', admin.site.urls),
+    path('pages/', include('pages.urls', namespace='pages')),
+    path('auth/registration/', UserCreateView.as_view(), name='registration'),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('blog.urls', namespace='blog')),
 ]
+
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.server_error'
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
