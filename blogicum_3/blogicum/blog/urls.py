c2429734e80902bb5c99994338a40e5ca27cb010
Aleksandr <sanchezzkusika@gmail.com>
2025-04-14 16:31:33 +0300

    added paths for post operations

--- blogicum_3/blogicum/blog/urls.py
+++ blogicum_3/blogicum/blog/urls.py
@@ -5,8 +5,10 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail,
-         name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts,
-         name='category_posts'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/',
+         views.category_posts, name='category_posts'),
+    path('create/', views.create_post, name='create_post'),
+    path('edit/<int:id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:id>/', views.delete_post, name='delete_post'),
 ]
