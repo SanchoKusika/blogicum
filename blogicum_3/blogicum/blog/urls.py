f5fc1b0a53f5b11e0f348461c5ebc9ed32d3c957
Aleksandr <sanchezzkusika@gmail.com>
2025-04-16 00:04:24 +0300

    updated urls.py

--- blogicum_3/blogicum/blog/urls.py
+++ blogicum_3/blogicum/blog/urls.py
@@ -1,64 +1,28 @@
-from django.urls import include, path
-from blog import views
+from django.urls import path
+from . import views
 
 app_name = 'blog'
 
-posts_urls = [
-    path(
-        '<int:post_id>/',
-        views.PostDetailView.as_view(),
-        name='post_detail'
-    ),
-    path(
-        'create/',
-        views.PostCreateView.as_view(),
-        name='create_post'
-    ),
-    path(
-        '<int:post_id>/edit/',
-        views.PostUpdateView.as_view(),
-        name='edit_post'
-    ),
-    path(
-        '<int:post_id>/delete/',
-        views.PostDeleteView.as_view(),
-        name='delete_post'
-    ),
-
-
-    path('<int:post_id>/comment/',
-         views.CommentCreateView.as_view(),
-         name='add_comment'),
-    path(
-        '<int:post_id>/edit_comment/<int:comment_id>/',
-        views.CommentUpdateView.as_view(),
-        name='edit_comment'
-    ),
-    path(
-        '<int:post_id>/delete_comment/<int:comment_id>/',
-        views.CommentDeleteView.as_view(),
-        name='delete_comment'
-    ),
-]
-
-
 urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
-    path(
-        'category/<slug:post_category>/',
-        views.CategoryListView.as_view(),
-        name='category_posts'
-    ),
-
-    path(
-        'profile/<slug:username>/',
-        views.ProfileDetailView.as_view(),
-        name='profile'
-    ),
-    path(
-        'edit_profile/',
-        views.UserUpdateView.as_view(),
-        name='edit_profile'
-    ),
-    path('posts/', include(posts_urls)),
+    path('posts/<int:post_id>/',
+         views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/edit/',
+         views.PostUpdateView.as_view(), name='edit_post'),
+    path('posts/<post_id>/delete/',
+         views.PostDeleteView.as_view(), name='delete_post'),
+    path('category/<slug:category_slug>/',
+         views.CategoryPostsView.as_view(), name='category_posts'),
+    path('profile/<str:username>/',
+         views.ProfileView.as_view(), name='profile'),
+    path('posts/create/',
+         views.PostCreateView.as_view(), name='create_post'),
+    path('accounts/profile/',
+         views.EditProfileView.as_view(), name='edit_profile'),
+    path('posts/<post_id>/comment/',
+         views.AddCommentView.as_view(), name='add_comment'),
+    path('posts/<post_id>/edit_comment/<comment_id>/',
+         views.EditCommentView.as_view(), name='edit_comment'),
+    path('posts/<post_id>/delete_comment/<comment_id>/',
+         views.DeleteCommentView.as_view(), name='delete_comment'),
 ]
