9fd47f3a88a519b512ed95a727616bda683e585e
Aleksandr <sanchezzkusika@gmail.com>
2025-04-15 23:58:24 +0300

    updated urls

--- blogicum_3/blogicum/pages/urls.py
+++ blogicum_3/blogicum/pages/urls.py
@@ -1,17 +1,22 @@
-from django.urls import path
-from pages import views
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
-app_name = 'pages'
 
-urlpatterns = [
-    path(
-        'about/',
-        views.AboutTemplateView.as_view(),
-        name='about'
-    ),
-    path(
-        'rules/',
-        views.RulesTemplateView.as_view(),
-        name='rules'
-    )
-]
+class RulesView(TemplateView):
+    template_name = 'pages/rules.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'pages/about.html'
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
