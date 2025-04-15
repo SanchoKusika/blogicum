983340a087ccb3965605ab7e5b52f8c4798fee44
Aleksandr <sanchezzkusika@gmail.com>
2025-04-16 00:53:35 +0300

    fixed urls in pages

--- blogicum_3/blogicum/pages/urls.py
+++ blogicum_3/blogicum/pages/urls.py
@@ -1,22 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import path
+from . import views
 
+app_name = 'pages'
+handler404 = 'pages.views.page_not_found'
 
-class RulesView(TemplateView):
-    template_name = 'pages/rules.html'
-
-
-class AboutView(TemplateView):
-    template_name = 'pages/about.html'
-
-
-def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=403)
-
-
-def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=404)
-
-
-def server_error(request):
-    return render(request, 'pages/500.html', status=500)
+urlpatterns = [
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('rules/', views.RulesView.as_view(), name='rules'),
+]
