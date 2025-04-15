ac8ddf85d11a26394578a6879be0225f7e210731
Aleksandr <sanchezzkusika@gmail.com>
2025-04-16 00:00:18 +0300

    updated urls

--- blogicum_3/blogicum/blogicum/urls.py
+++ blogicum_3/blogicum/blogicum/urls.py
@@ -1,24 +1,33 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import UserCreateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+handler404 = 'pages.views.page_not_found'
+
+handler500 = 'pages.views.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls', namespace='pages')),
-    path('auth/registration/', UserCreateView.as_view(), name='registration'),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('blog.urls', namespace='blog')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration',
+    ),
 ]
 
-handler404 = 'pages.views.page_not_found'
-handler500 = 'pages.views.server_error'
 
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
