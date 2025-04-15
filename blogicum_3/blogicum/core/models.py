2c42bcb412fffe8377c89100ce563c1fa0232825
Aleksandr <sanchezzkusika@gmail.com>
2025-04-15 23:55:53 +0300

    models rewritten

--- blogicum_3/blogicum/core/models.py
+++ blogicum_3/blogicum/core/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 
-class PublishedModel(models.Model):
+class BaseModel(models.Model):
+
     is_published = models.BooleanField(
-        'Опубликовано',
+        verbose_name='Опубликовано',
         default=True,
         help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
     created_at = models.DateTimeField(
-        'Добавлено',
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name='Добавлено',
     )
 
     class Meta:
